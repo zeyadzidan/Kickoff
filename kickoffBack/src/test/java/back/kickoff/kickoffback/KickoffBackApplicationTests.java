3124daed37abb9fbce274f601ef944558807bfad
Omar khairat <omarkhairat133@gmail.com>
2022-11-28 12:09:41 +0200

    packages created

---
+++ kickoffBack/src/test/java/back/kickoff/kickoffback/KickoffBackApplicationTests.java
@@ -0,0 +1,13 @@
+package back.kickoff.kickoffback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickoffBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
