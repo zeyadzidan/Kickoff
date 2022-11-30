e1bc75912d9e43447ae7ff5c615ad02994ee962f
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-11-30 17:15:26 +0200

    Introduced light and dark themes

---
+++ kickoff_frontend/lib/themes.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+enum Themes {light, dark}
+
+class AppThemes {
+
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: Colors.green,
+    appBarTheme: const AppBarTheme(
+        color: Colors.green
+    ),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: Colors.grey,
+      cursorColor: Colors.black,
+      selectionHandleColor: Colors.greenAccent,
+    ),
+    backgroundColor: Colors.white70,
+    dividerColor: Colors.green,
+    brightness: Brightness.light,
+    highlightColor: Colors.white,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData (
+        backgroundColor: Colors.green,
+        focusColor: Colors.greenAccent,
+        splashColor: Colors.lightGreen
+    ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primaryColor: Colors.greenAccent,
+    scaffoldBackgroundColor: Colors.black54,
+    dividerColor: Colors.greenAccent,
+    brightness: Brightness.dark,
+    highlightColor: Colors.white,
+    backgroundColor: Colors.black87,
+    textSelectionTheme: const TextSelectionThemeData(
+        selectionColor: Colors.grey
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData (
+        foregroundColor: Colors.black87,
+        backgroundColor: Colors.green,
+        focusColor: Colors.greenAccent,
+        splashColor: Colors.lightGreen
+    ),
+  );
+
+  static ThemeData getThemeFromKey(Themes key) {
+    switch (key) {
+      case Themes.light:
+        return lightTheme;
+      case Themes.dark:
+        return darkTheme;
+      default:
+        return lightTheme;
+    }
+  }
+}
