0a3c1455b671cd618e745e36cbb80e72cdff7868
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-11-30 17:14:46 +0200

    Fixture panel builder

---
+++ kickoff_frontend/lib/fixtures/builders/fpbuilder.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// Fixture panel builder class to build a fixture panel in a panel-list.
+class FixturePanelBuilder {
+  ExpansionPanel build(String header, String body, bool isExpanded) => ExpansionPanel(
+      headerBuilder: (_, isExpanded) => Container(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        child: Text(header)
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+        child: Text(body)
+      ),
+      isExpanded: isExpanded,
+      canTapOnHeader: true,
+    );
+}
