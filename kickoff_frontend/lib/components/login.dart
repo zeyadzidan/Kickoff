d272b31263611818973264a257469aebaf341322
Mento79 <bodyomar79@gmail.com>
2022-12-02 21:33:27 +0200

    trying stay login

--- kickoff_frontend/lib/components/login.dart
+++ kickoff_frontend/lib/components/login.dart
@@ -2,82 +2,90 @@ import 'dart:convert';
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:kickoff_frontend/application.dart';
+import 'package:kickoff_frontend/components/rounded_button.dart';
 import 'package:kickoff_frontend/components/rounded_input.dart';
 import 'package:kickoff_frontend/constants.dart';
 import 'package:kickoff_frontend/components/rounded_input_login.dart';
 import 'package:kickoff_frontend/components/rounded_password_input.dart';
 import 'package:http/http.dart' as http;
-class LogiButton extends StatefulWidget
-{
+
+class LogiButton extends StatefulWidget {
   @override
   RoundedLogin createState() => RoundedLogin();
 }
+
 class RoundedLogin extends State<LogiButton> {
   String url = "http://192.168.1.2:8080/login/courtOwner";
-  //String url= "https://student.valuxapps.com/api/login";
-  var resp=52;
+  static String url2 = "http://192.168.1.2:8080/login/courtOwner";
+  var resp = 52;
   late Map<String, dynamic> Profile_data;
-  Future save() async{
+  Future save(email, pass) async {
     print(RoundedInput.EmailSignUp.text);
-    var res= await http.post(Uri.parse(url),headers: {"Content-Type": "application/json"},body:json.encode(
-        {
-          "email": RoundedInputLogin.EmailLogin.text,
-          "password": RoundedPasswordInput.Password.text,
-        })
-    );
+    var res = await http.post(Uri.parse(url),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({
+          "email": email,
+          "password": pass,
+        }));
     setState(() {
-       Profile_data=jsonDecode(res.body);
-       // print(Profile_data);
+      Profile_data = json.decode(res.body);
     });
     print(res.body);
   }
+
+  static Future save2(email, pass) async {
+    var res = await http.post(Uri.parse(url2),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Allow-Headers":
+              "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+          "Access-Control-Allow-Methods": "POST, OPTIONS"
+        },
+        body: json.encode({
+          "email": email,
+          "password": pass,
+        }));
+    print(res.body);
+    return json.decode(res.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return InkWell(
       onTap: () async {
-          var Email = RoundedInputLogin.EmailLogin.text;
-          var Password=RoundedPasswordInput.Password.text;
-          print(Email);
-          print(Password);
-          if(Email.isEmpty )
-          {
-            showAlertDialog(context,'Enter valid Email');
+        var Email = RoundedInputLogin.EmailLogin.text;
+        var Password = RoundedPasswordInput.Password.text;
+        print(Email);
+        print(Password);
+        if (Email.isEmpty) {
+          showAlertDialog(context, 'Enter valid Email');
+          RoundedInputLogin.EmailLogin.clear();
+        } else if (Password.length < 6 ||
+            Password.length > 15 ||
+            Password.isEmpty) {
+          showAlertDialog(context, 'Enter Valid Password');
+          RoundedPasswordInput.Password.clear();
+        } else {
+          var res = await save(RoundedInputLogin.EmailLogin.text,
+              RoundedPasswordInput.Password.text);
+          print(Profile_data.length);
+          if (Profile_data.length == 0) {
+            showAlertDialog(context, 'Enter valid Email');
             RoundedInputLogin.EmailLogin.clear();
-          }
-          else if( Password.length<6 || Password.length>15 || Password.isEmpty)
-          {
-            showAlertDialog(context,'Enter Valid Password');
+          } else if (Profile_data.length == 4) {
+            showAlertDialog(context, 'Enter valid Password');
             RoundedPasswordInput.Password.clear();
+          } else {
+            print(Profile_data);
+            // print("\t"+Profile_data);
+            KickoffApplication.profileData = Profile_data;
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => KickoffApplication()));
           }
-          else
-            {
+          /*
 
-              var res= await save();
-              print(Profile_data.length);
-              if(Profile_data.length==0)
-                {
-                  showAlertDialog(context,'Enter valid Email');
-                  RoundedInputLogin.EmailLogin.clear();
-
-                }
-              else if(Profile_data.length==4)
-                {
-                  showAlertDialog(context,'Enter valid Password');
-                  RoundedPasswordInput.Password.clear();
-                }
-              else
-                {
-                  print(Profile_data);
-                  // print("\t"+Profile_data);
-                  KickoffApplication.profileData=Profile_data;
-                  Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => KickoffApplication()
-                      )
-                  );
-                }
-              /*
               if(resp==0)
               {
                 showAlertDialog(context,'Enter valid Data');
@@ -90,31 +98,27 @@ class RoundedLogin extends State<LogiButton> {
 
               }
                */
-            }
+        }
       },
       borderRadius: BorderRadius.circular(30),
-
       child: Container(
         width: size.width * 0.8,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
           color: kPrimaryColor,
         ),
-
         padding: EdgeInsets.symmetric(vertical: 20),
         alignment: Alignment.center,
         child: Text(
           'LOGIN',
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 18
-          ),
+          style: TextStyle(color: Colors.white, fontSize: 18),
         ),
       ),
     );
   }
 }
-showAlertDialog(BuildContext context,text3) {
+
+showAlertDialog(BuildContext context, text3) {
   // Create button
   Widget okButton = TextButton(
     child: Text("OK"),
--- kickoff_frontend/lib/main.dart
+++ kickoff_frontend/lib/main.dart
@@ -2,37 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:kickoff_frontend/Screens/login/login.dart';
 import 'package:kickoff_frontend/constants.dart';
 import 'package:kickoff_frontend/application.dart';
+import 'package:kickoff_frontend/localFile.dart';
+import 'package:kickoff_frontend/components/login.dart';
 
-
-void main() => runApp(
-  const MyApp()
-);
-
+String loginData = "";
+bool first_time = true;
+void main() async {
+  runApp(const MyApp());
+  loginData = await localFile.readLoginData();
+  first_time = (loginData == "0");
+  print(loginData);
+  print(first_time);
+  // if (!first_time) {
+  //   int idx = loginData.indexOf(":");
+  //   String email = loginData.substring(0, idx).trim();
+  //   String pass = loginData.substring(idx + 1).trim();
+  //   print("email");
+  //   print(email);
+  //   print("pass");
+  //   print(pass);
+  //   String url = "http://localhost:8080/login/courtOwner";
+  //
+  //   KickoffApplication.profileData = await RoundedLogin.save2(email, pass);
+  // }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Animated Login',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        primaryColor: kPrimaryColor,
-      ),
-      // bottomNavigationBar: NavBar.navBar(),
-      home: LoginScreen(),
-    );
+    return first_time
+        ? MaterialApp(
+            title: 'Flutter Animated Login',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: Colors.green,
+              primaryColor: kPrimaryColor,
+            ),
+            // bottomNavigationBar: NavBar.navBar(),
+            home: LoginScreen(),
+          )
+        : MaterialApp(
+            title: 'Flutter Animated Login',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: Colors.green,
+              primaryColor: kPrimaryColor,
+            ),
+            // bottomNavigationBar: NavBar.navBar(),
+            home: KickoffApplication(),
+          );
   }
 }
