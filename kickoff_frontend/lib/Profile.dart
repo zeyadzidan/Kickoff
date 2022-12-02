1e39476186d980f9056a2d507dd1328e8aaef77f
Mento79 <bodyomar79@gmail.com>
2022-12-02 02:27:00 +0200

    add some profile functionalities

--- kickoff_frontend/lib/Profile.dart
+++ kickoff_frontend/lib/Profile.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class ProfileBaseScreen extends StatefulWidget {
   const ProfileBaseScreen({Key? key}) : super(key: key);
@@ -12,8 +16,12 @@ class _ProfileBaseScreenState extends State<ProfileBaseScreen> {
   int subscribers = 0;
   String name = "Barcelona Stadiums";
   String phone = "01234567897";
-  String map =
+  String address =
       "https://www.google.com/maps/d/viewer?mid=1KiMxuAuJbF_CXsxYDub13OpSZUk";
+  bool emptyphoto = false;
+  double xaxis = 31.2160786;
+  double yaxis = 29.9469253;
+  String? path;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,12 +41,42 @@ class _ProfileBaseScreenState extends State<ProfileBaseScreen> {
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const CircleAvatar(
-                            radius: 40,
-                            backgroundColor: Color(0xff74EDED),
-                            backgroundImage: AssetImage('images/barcelona.png')
-                            //     NetworkImage("https://placeimg.com/640/480/people"),
+                        if (emptyphoto) ...[
+                          CircleAvatar(
+                              radius: 40,
+                              backgroundColor: Color(0xff74EDED),
+                              backgroundImage: Image.file(File(path!)).image
+                                  as ImageProvider<Object>
+                              //     NetworkImage("https://placeimg.com/640/480/people"),
+                              )
+                        ] else ...[
+                          MaterialButton(
+                            onPressed: () async {
+                              FilePickerResult? result =
+                                  await FilePicker.platform.pickFiles(
+                                      type: FileType.custom,
+                                      allowedExtensions: ['png', 'jpg']);
+
+                              // The result will be null, if the user aborted the dialog
+                              if (result != null) {
+                                File file = File(result.files.first.path!);
+                                path = result.files.first.path;
+                              }
+                              setState(() {
+                                path = result?.files.first.path;
+                                emptyphoto = true;
+                              });
+                            },
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: Icon(
+                              Icons.add,
+                              size: 40,
                             ),
+                            padding: EdgeInsets.all(20),
+                            shape: CircleBorder(),
+                          )
+                        ],
                         Padding(
                           padding: const EdgeInsets.fromLTRB(0, 12, 20, 0),
                           child: Row(children: [
@@ -137,14 +175,17 @@ class _ProfileBaseScreenState extends State<ProfileBaseScreen> {
                         ),
                         Align(
                           alignment: Alignment.centerLeft,
-                          child: Text(
-                            " \u{1F5FA} ${map}",
-                            style: TextStyle(
-                              letterSpacing: 0.4,
-                              fontSize: 10,
-                              // color: ,
-                            ),
-                          ),
+                          child: InkWell(
+                              child: Text(
+                                " \u{1F5FA} ${address}",
+                                style: TextStyle(
+                                  letterSpacing: 0.4,
+                                  fontSize: 10,
+                                  // color: ,
+                                ),
+                              ),
+                              onTap: () => launchUrlString(
+                                  'https://www.google.com/maps/place/${xaxis}+${yaxis}')),
                         ),
                       ],
                     ),
