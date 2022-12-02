496f12289f6a424ad3bbac1f00cd790c46ad4b37
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-02 02:57:44 +0200

    Prepared an application runner that runs our Kickoff Application

--- kickoff_frontend/lib/fixtures/widgets/application.dart
+++ kickoff_frontend/lib/fixtures/widgets/application.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:kickoff_frontend/fixtures/widgets/addfixturebutton.dart';
 import 'package:kickoff_frontend/fixtures/widgets/reservations.dart';
 import 'package:kickoff_frontend/themes.dart';
 
@@ -8,7 +7,7 @@ class KickoffApplication extends StatefulWidget {
   const KickoffApplication({super.key});
 
   static final List pages = [
-    const AddFixtureButton(),
+    const Center(child: Text("DUMMY PROFILE PAGE")),
     const Center(child: Text("ANNOUNCEMENTS FEATURE IS NOT YET IMPLEMENTED")),
     ReservationsHome(MyInfo.info)
   ];
@@ -19,11 +18,13 @@ class KickoffApplication extends StatefulWidget {
 
 class KickoffApplicationState extends State<KickoffApplication> {
 
-  static int selectedPage = 0;
+  late TimeOfDay _initSelectedTime = TimeOfDay.now();
+  late TimeOfDay _finSelectedTime = TimeOfDay.now();
+  int _selectedPage = 0;
 
   _onTapSelect(index) =>
       setState(
-              () => selectedPage = index
+              () => _selectedPage = index
       );
 
   @override
@@ -32,13 +33,16 @@ class KickoffApplicationState extends State<KickoffApplication> {
         theme: AppThemes.lightTheme,
         title: "Kickoff",
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: _buildAppBar(),
-          body: Center(
-            child: KickoffApplication.pages[selectedPage],
-          ),
-          floatingActionButton: (selectedPage == 2) ? _buildAddFixtureButton() : null,
-          bottomNavigationBar: _buildNavBar()
+        home: Builder (
+          builder: (context) =>
+              Scaffold(
+                appBar: _buildAppBar(),
+                body: Center(
+                  child: KickoffApplication.pages[_selectedPage],
+                ),
+                floatingActionButton: (_selectedPage == 2) ? _buildAddFixtureButton(context) : null,
+                bottomNavigationBar: _buildNavBar(),
+              ),
         )
     );
   }
@@ -54,94 +58,112 @@ class KickoffApplicationState extends State<KickoffApplication> {
     backgroundColor: Colors.green,
   );
 
-  late String _selectedTime = TimeOfDay.now().format(context);
-
-  _pickTime() async {
-    final String? timeOfDay = (await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    ))?.format(context);
-
-    if (timeOfDay != _selectedTime) {
-      setState(() => _selectedTime = timeOfDay!);
-    }
-  }
-
-  _buildAddFixtureButton() => FloatingActionButton(
-      onPressed: () {
-        GlobalKey<FormState> formKey = GlobalKey();
-        List ticketInformation = <String>[];
-        showModalBottomSheet(
-          context: context,
-          builder: (BuildContext context) => SingleChildScrollView(
-            child: SizedBox(
-              height: 300,
-              child: Form(
-                key: formKey,
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          keyboardType: TextInputType.name,
-                          validator: (input) {
-                            if (input == null) {
-                              return 'Name cannot be blank';
-                            } else {
-                              formKey.currentState?.save();
+  _buildAddFixtureButton(context) => Builder(
+      builder: (context) {
+        GlobalKey<FormState> key = GlobalKey();
+        List<String> ticketInfo = <String>[];
+        return FloatingActionButton(
+          onPressed: () =>
+              showModalBottomSheet(
+                elevation: 4,
+                context: context,
+                builder: (context) => SizedBox(
+                  height: 200,
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: key,
+                      child: Column(
+                        children: [
+                          // TODO: HANDLE EMPTY PLAYER NAME CASE
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                prefixIcon: Icon(Icons.person),
+                                hintText: "Player Name"
+                            ),
+                            keyboardType: TextInputType.name,
+                            validator: (input) {
+                              if (input == null)
+                                return "Name can not be blank";
+                              key.currentState?.save();
                               return null;
-                            }
-                          },
-                          decoration: const InputDecoration(
-                            hintText: "Player Name",
-                            prefixIcon: Icon(Icons.person),
+                            },
+                            onSaved: (value) => ticketInfo.add(value!),
                           ),
-                          onSaved: (value) => ticketInformation.add(value),
-                        ),
-                        MaterialButton(
-                          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-                          onPressed: _pickTime,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const Icon(Icons.timer),
-                              Text('  Picked time: $_selectedTime'),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          alignment: Alignment.bottomCenter,
-                          child: ElevatedButton(
-                              onPressed: () {
-                                String court = KickoffApplication.pages[KickoffApplicationState.selectedPage].selectedCourt;
-                                String date = KickoffApplication.pages[KickoffApplicationState.selectedPage].selectedDate;
-                                ticketInformation.add(court);
-                                ticketInformation.add(date);
-
-                                formKey.currentState?.save();
-
-                                print(ticketInformation);
-
-                                // TODO: SEND THE TICKET INFO TO BACK END
-                                print(ticketInformation);
+                          // TODO: ACCEPT HOUR ONLY
+                          MaterialButton(
+                            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+                            onPressed: () async {
+                              var time =  await showTimePicker(
+                                initialEntryMode: TimePickerEntryMode.inputOnly,
+                                initialTime: TimeOfDay.now(),
+                                builder: (BuildContext context, Widget? child) {
+                                  return MediaQuery(
+                                    data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+                                    child: child!,
+                                  );
+                                },
+                                context: context
+                                );
+                              setState(() => _initSelectedTime = time!);
                               },
-                              child: const Text("SUBMIT")
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(Icons.timer),
+                                Text('  From: ${_initSelectedTime.format(context)}')
+                              ],
+                            ),
                           ),
-                        )
-                      ],
-                    )
-                ),
+                          MaterialButton(
+                            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+                            onPressed: () async {
+                              var time =  await showTimePicker(
+
+                                initialTime: TimeOfDay.now(),
+                                context: context
+                                );
+                              setState(() => _finSelectedTime = time!);
+                              },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(Icons.timer),
+                                Text('  To: ${_finSelectedTime.format(context)}')
+                              ],
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                key.currentState?.save();
+
+                                String court = 'Court: ${KickoffApplication.pages[_selectedPage].selectedCourt}';
+                                String date = KickoffApplication.pages[_selectedPage].selectedDate;
+
+                                ticketInfo.add(court);
+                                ticketInfo.add(date);
+                                ticketInfo.add(_initSelectedTime.format(context));
+                                ticketInfo.add(_finSelectedTime.format(context));
+
+                                print(ticketInfo);  // Send the creation request to back-end
+                              },
+                              child: const Text("SUBMIT"),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
               ),
-            ),
-          ),
-          elevation: 4,
+            elevation: 4,
+            foregroundColor: Theme.of(context).floatingActionButtonTheme.foregroundColor,
+            backgroundColor: Theme.of(context).floatingActionButtonTheme.backgroundColor,
+            hoverColor: Colors.green.shade800,
+            child: const Icon(Icons.add, size: 35)
         );
-      },
-      foregroundColor: Theme.of(context).floatingActionButtonTheme.foregroundColor,
-      backgroundColor: Theme.of(context).floatingActionButtonTheme.backgroundColor,
-      hoverColor: Colors.green.shade800,
-      hoverElevation: 4,
-      child: const Icon(Icons.add, size: 35)
+      }
   );
 
   _buildNavBar() => GNav(
@@ -167,7 +189,7 @@ class KickoffApplicationState extends State<KickoffApplication> {
           icon: Icons.stadium,
         ),
       ],
-      selectedIndex: selectedPage,
+      selectedIndex: _selectedPage,
       onTabChange: _onTapSelect
   );
 }
