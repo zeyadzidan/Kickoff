61b2d0c85c356b4e86d40b7bce808ac74622c9bf
Omar khairat <omarkhairat133@gmail.com>
2022-12-05 04:47:46 +0200

    All test added except courtAgent

---
+++ kickoffBack/src/test/java/back/kickoff/kickoffback/cotrollers/SignupControllerTest.java
@@ -0,0 +1,48 @@
+package back.kickoff.kickoffback.cotrollers;
+
+import back.kickoff.kickoffback.services.SignupService;
+import com.google.gson.Gson;
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class SignupControllerTest {
+    SignupController controller;
+    @Mock
+    SignupService signupService;
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+        controller = new SignupController(signupService);
+    }
+    @Test
+    void courtOwnerSignupRequest() throws JSONException {
+        HashMap<String, Object> hm = new HashMap<>();
+        hm.put("email", "nasrClub@gmail.com");
+        hm.put("password", "12345678900");
+        hm.put("username", "Nasr CLub");
+        hm.put("location", "Nasr CLub");
+        hm.put("phoneNumber", "01206555589");
+        hm.put("xAxis", 44.5);
+        hm.put("yAxis", 44.5);
+        String information = new Gson().toJson(hm);
+        when(signupService.courtOwnerSignup(information)).thenReturn(1);
+        ResponseEntity<String> res =controller.courtOwnerSignupRequest(information);
+        assertEquals(res, new ResponseEntity<>("1", HttpStatus.CREATED));
+    }
+}
---
+++ kickoffBack/src/test/java/back/kickoff/kickoffback/model/CourtOwnerTest.java
@@ -0,0 +1,73 @@
+package back.kickoff.kickoffback.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CourtOwnerTest {
+    CourtOwner courtOwner;
+
+    @BeforeEach
+    void setUp() {
+        courtOwner = new CourtOwner();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        courtOwner.setId(idValue);
+        assertEquals(idValue, courtOwner.getId());
+    }
+
+    @Test
+    void getUserName() {
+        String userName = "Nasr Club";
+        courtOwner.setUserName(userName);
+        assertEquals(userName, courtOwner.getUserName());
+    }
+
+    @Test
+    void getEmail() {
+        String email = "nasrClub@gmail.com";
+        courtOwner.setEmail(email);
+        assertEquals(email, courtOwner.getEmail());
+    }
+
+    @Test
+    void getPassword() {
+        String password = "123456789010";
+        courtOwner.setPassword(password);
+        assertEquals(password, courtOwner.getPassword());
+    }
+
+    @Test
+    void getLocation() {
+        String location = "El-Nasr Sporting Club, 483H+WCC, Ahmed Ali Hussein, El-Nozha, El Nozha, Cairo Governorate 4470011";
+        courtOwner.setLocation(location);
+        assertEquals(location, courtOwner.getLocation());
+    }
+
+    @Test
+    void getRating() {
+        float rating = 0.2f;
+        courtOwner.setRating(rating);
+        assertEquals(rating, courtOwner.getRating());
+    }
+
+    @Test
+    void getImage() {
+        String image = "www.imageClub.com/1234.png";
+        courtOwner.setImage(image);
+        assertEquals(image, courtOwner.getImage());
+    }
+
+}
