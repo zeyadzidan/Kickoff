725b6cc76f490f826e69a9f07d15b3a9804bddc7
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-09 03:27:45 +0200

    Huge clean up and refactoring to main class

--- kickoff_frontend/lib/main.dart
+++ kickoff_frontend/lib/main.dart
@@ -1,44 +1,15 @@
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kickoff_frontend/application/application.dart';
 import 'package:kickoff_frontend/constants.dart';
-import 'package:kickoff_frontend/localFile.dart';
 
+import 'application/screens/dataloading.dart';
 import 'application/screens/login.dart';
-import 'application/screens/reservations.dart';
-import 'httpshandlers/courtsrequests.dart';
-import 'httpshandlers/loginrequests.dart';
 
-String loginData = "";
-bool loading = true;
-bool firstTime = true;
-bool finish = false;
-Map<String, dynamic> data = {};
-//
-// late Map<String, dynamic> profileData;
-//
-Future main() async {
-  // String getIP = (await NetworkInfo().getWifiIP())!;
+void main() {
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
-  runApp(const MyApp());
-  loginData = await localFile.readLoginData();
-  firstTime = (loginData == "0");
-  loading = false;
-  if (!firstTime) {
-    int idx = loginData.indexOf(":");
-    String email = loginData.substring(0, idx).trim();
-    String pass = loginData.substring(idx + 1).trim();
-    data = await RoundedLogin.save2(email, pass);
-    int id = data["id"];
-    KickoffApplication.courts = await CourtsHTTPsHandler.getCourts(id);
-    await ReservationsHome.buildTickets("main");
-    finish = true;
-  } else {
-    finish = true;
-  }
+  Loading.loadData();
+  runApp(KickoffApplication(profileData: data));
 }
 
 class MyApp extends StatefulWidget {
@@ -49,26 +20,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 0;
-  late Timer _timer;
-  updateCounter() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      counter++;
-      setState(() {
-        if (loginData != "" && finish) {
-          firstTime = (loginData == "0");
-          loading = false;
-          _timer.cancel();
-        }
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (loginData == "") {
-      updateCounter();
-    }
     return loading
         ? MaterialApp(
             title: 'KickOff',
@@ -84,70 +37,20 @@ class _MyAppState extends State<MyApp> {
               // Notice that the counter didn't reset back to zero; the application
               // is not restarted.
               primarySwatch: Colors.green,
-              primaryColor: kPrimaryColor,
+              primaryColor: primaryColor,
             ),
             home: Container(
               color: Colors.white,
-              child: Stack(children: [
-                Positioned(
-                    top: 100,
-                    right: -50,
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: const <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 5,
-                            ),
-                          ],
-                          color: kPrimaryColor),
-                    )),
-                Positioned(
-                    top: -50,
-                    left: -50,
-                    child: Container(
-                      width: 190,
-                      height: 190,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(95),
-                          boxShadow: const <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 5,
-                            ),
-                          ],
-                          color: kPrimaryColor),
-                    )),
-                Positioned(
-                    bottom: -100,
-                    left: -100,
-                    child: Container(
-                      width: 300,
-                      height: 300,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(150),
-                          boxShadow: const <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 5,
-                            ),
-                          ],
-                          color: kPrimaryColor),
-                    )),
-                Center(
-                    child: SizedBox(
-                  width: 200,
-                  height: 200,
-                  child: Container(
-                      height: 175,
-                      width: 175,
-                      child: const Image(
-                          image: AssetImage('assets/images/pic4.PNG'))),
-                )),
-              ]),
+              child: Center(
+                  child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Container(
+                    height: 175,
+                    width: 175,
+                    child: const Image(
+                        image: AssetImage('assets/images/pic4.PNG'))),
+              )),
             ),
           )
         : MaterialApp(
