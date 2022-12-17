69cfd5f5c0da8a70ff8037eff6c5bd3d7b2b666c
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-17 02:34:23 +0200

    Announcement Frontend Model

--- kickoff_frontend/lib/components/classes/announcement.dart
+++ kickoff_frontend/lib/components/classes/announcement.dart
@@ -1,10 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
+
 
 class Announcement {
   String coid = '';
   String aid = '';
   String title = '';
   String body = '';
-  CachedNetworkImage cni = CachedNetworkImage(imageUrl: '');
+  String img = '';
   String date = '';
 }
