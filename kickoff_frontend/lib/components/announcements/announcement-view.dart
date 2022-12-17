73de60fb110ebb5d3450626fab9c42abad30e0c9
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-17 02:33:24 +0200

    Announcements View v2.0
    Modified the announcements view and extended functionality. Still needs to resolve the state update on adding.

--- kickoff_frontend/lib/components/announcements/announcement-view.dart
+++ kickoff_frontend/lib/components/announcements/announcement-view.dart
@@ -6,7 +6,7 @@ import 'package:kickoff_frontend/httpshandlers/announcements-requests.dart';
 import '../../constants.dart';
 
 class AnnouncementsView extends StatefulWidget {
-  AnnouncementsView({super.key});
+  const AnnouncementsView({super.key});
 
   @override
   State<StatefulWidget> createState() => _AnnouncementsViewState();
@@ -14,63 +14,82 @@ class AnnouncementsView extends StatefulWidget {
 
 class _AnnouncementsViewState extends State<AnnouncementsView> {
   @override
-  Widget build(BuildContext context) => Expanded(
-        child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: ExpansionPanelList(
-              animationDuration: const Duration(milliseconds: 300),
-              expandedHeaderPadding: EdgeInsets.zero,
-              dividerColor: primaryColor,
-              elevation: 4,
-              children: List<ExpansionPanel>.generate(
-                  AnnouncementsHome.announcements.length,
-                  (index) => ExpansionPanel(
-                        headerBuilder: (_, isExpanded) => Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 30),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          child: Text(
-                              AnnouncementsHome.announcements[index].title),
-                        ),
-                        body: Container(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 50, horizontal: 30),
-                            child: Column(
-                              children: [
-                                Text(AnnouncementsHome
-                                    .announcements[index].body),
-                                Container(
-                                  margin: const EdgeInsets.only(
-                                      top: 70.0, right: 300.0),
-                                  child: ElevatedButton.icon(
-                                    icon: const Icon(Icons.delete),
-                                    label: const Text('مسح الإعلان'),
-                                    style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.red,
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 20, horizontal: 15)),
-                                    onPressed: () async {
-                                      await AnnouncementHTTPsHandler
-                                          .deleteAnnouncement(AnnouncementsHome
-                                              .announcements[index]);
-                                      AnnouncementsHome.announcements =
-                                          await AnnouncementHTTPsHandler
-                                              .getAnnouncements(
-                                                  KickoffApplication.ownerId);
-                                      KickoffApplication.update();
-                                    },
+  Widget build(BuildContext context) => SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: ExpansionPanelList(
+        animationDuration: const Duration(milliseconds: 300),
+        expandedHeaderPadding: EdgeInsets.zero,
+        dividerColor: primaryColor,
+        elevation: 4,
+        children: List<ExpansionPanel>.generate(
+            AnnouncementsHome.announcements.length,
+            (index) => ExpansionPanel(
+                  headerBuilder: (_, isExpanded) => Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 30),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Text(AnnouncementsHome.announcements[index].title),
+                  ),
+                  body: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 50, horizontal: 30),
+                      child: Column(
+                        children: [
+                          Text(AnnouncementsHome.announcements[index].body),
+                          Image.network(
+                            AnnouncementsHome.announcements[index].img,
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                            frameBuilder: (context, child, frame,
+                                wasSynchronouslyLoaded) {
+                              return child;
+                            },
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress == null) {
+                                return child;
+                              } else {
+                                return const Center(
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
                                   ),
-                                ),
-                              ],
-                            )),
-                        isExpanded: AnnouncementsHome.isExpanded[index],
-                        canTapOnHeader: true,
+                                );
+                              }
+                            },
+                          ),
+                          Container(
+                            margin:
+                                const EdgeInsets.only(top: 70.0, right: 300.0),
+                            child: ElevatedButton.icon(
+                              icon: const Icon(Icons.delete),
+                              label: const Text('مسح الإعلان'),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.red,
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 20, horizontal: 15)),
+                              onPressed: () async {
+                                await AnnouncementHTTPsHandler
+                                    .deleteAnnouncement(AnnouncementsHome
+                                        .announcements[index].aid);
+                                AnnouncementsHome.announcements =
+                                    await AnnouncementHTTPsHandler
+                                        .getAnnouncements(
+                                            KickoffApplication.ownerId);
+                                setState(() {
+
+                                });
+                              },
+                            ),
+                          ),
+                        ],
                       )),
-              expansionCallback: (i, isExpanded) =>
-                  setState(() => AnnouncementsHome.isExpanded[i] = !isExpanded),
-            )),
-      );
+                  isExpanded: AnnouncementsHome.isExpanded[index],
+                  canTapOnHeader: true,
+                )),
+        expansionCallback: (i, isExpanded) =>
+            setState(() => AnnouncementsHome.isExpanded[i] = !isExpanded),
+      ));
 }
