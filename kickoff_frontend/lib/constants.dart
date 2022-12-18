bff96420baf14479cae462319cef7c17b22a77d4
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-18 15:04:29 +0200

    Added custom toast

--- kickoff_frontend/lib/constants.dart
+++ kickoff_frontend/lib/constants.dart
@@ -6,4 +6,16 @@ const secondaryColor = Colors.white;
 const alternativeColor = Colors.black87;
 const ip = "192.168.1.9";
 
+customToast(text) => Container(
+  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(25.0),
+    color: Colors.green,
+  ),
+  child: Text(
+    text,
+    style: const TextStyle(color: secondaryColor),
+  ),
+);
+
 getIP() => ip;
