9779d5ea39c6dc3d9f2db0132f97975a704edec1
abdelrahman316gad <abdelrahmanfencergadafc@gmail.com>
2022-12-29 15:34:30 +0200

    Revert "Added a custom query"
    This reverts commit 525abc2726e8fe5131ac31968dc8414ace4ee3c3.

--- kickoffBack/src/main/java/back/kickoff/kickoffback/repositories/ReservationRepository.java
+++ kickoffBack/src/main/java/back/kickoff/kickoffback/repositories/ReservationRepository.java
@@ -2,16 +2,12 @@ package back.kickoff.kickoffback.repositories;
 
 import back.kickoff.kickoffback.model.Reservation;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Component
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
-    public List<Reservation> findAllByPid(@Param("pid") Long pid);
 //    public boolean existsById(Long id);
 }
