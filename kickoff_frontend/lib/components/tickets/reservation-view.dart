9edafa1ec334b55e6c6d90291858dd13a25ef1c7
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-30 00:49:59 +0200

    reservations view modifications

--- kickoff_frontend/lib/components/tickets/reservation-view.dart
+++ kickoff_frontend/lib/components/tickets/reservation-view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:kickoff_frontend/application/application.dart';
@@ -158,6 +159,31 @@ class _ReservationsViewState extends State<ReservationsView> {
                                     : Container(
                                         height: 0,
                                       ),
+                                KickoffApplication.player &&
+                                        ReservationsHome
+                                                .reservations[index].state ==
+                                            'Awaiting'
+                                    ? Container()
+                                    : ReservationsHome.reservations[index]
+                                                .receiptUrl !=
+                                            ""
+                                        ? CachedNetworkImage(
+                                            imageUrl: ReservationsHome
+                                                .reservations[index].receiptUrl,
+                                            width: double.infinity,
+                                            fit: BoxFit.cover,
+                                            progressIndicatorBuilder: (context,
+                                                    url, downloadProgress) =>
+                                                CircularProgressIndicator(
+                                                    value: downloadProgress
+                                                        .progress),
+                                            errorWidget:
+                                                (context, url, error) =>
+                                                    const Icon(Icons.error),
+                                          )
+                                        : Container(
+                                            height: 0,
+                                          ),
                                 KickoffApplication.player
                                     ? Container()
                                     : (ReservationsHome.reservations[index]
