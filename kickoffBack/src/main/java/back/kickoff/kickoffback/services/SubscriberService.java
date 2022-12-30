1818d111711ff686a83bc6d32a6eca93e23149d7
Zeyad Zidan <iamzeyadzidan@gmail.com>
2022-12-30 16:48:12 +0200

    Adapted the service to the new controller functionalities

--- kickoffBack/src/main/java/back/kickoff/kickoffback/services/SubscriberService.java
+++ kickoffBack/src/main/java/back/kickoff/kickoffback/services/SubscriberService.java
@@ -2,8 +2,6 @@ package back.kickoff.kickoffback.services;
 
 import back.kickoff.kickoffback.model.Subscription;
 import back.kickoff.kickoffback.repositories.SubscriptionsRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,16 +16,12 @@ public class SubscriberService {
     }
 
     public boolean subscribe(Subscription subscription) {
-        try {
-            // Checking whether a subscription is present or not.
-            if (subscriptionsRepository.findByPid(subscription.getPid()).contains(subscription) &&
-                    subscriptionsRepository.findByCoid(subscription.getCoid()).contains(subscription))
-                return false;
-            subscriptionsRepository.save(subscription);
-            return true;
-        } catch (Exception ignored) {
+        // Checking whether a subscription is present or not.
+        if (subscriptionsRepository.findByPid(subscription.getPid()).contains(subscription) &&
+                subscriptionsRepository.findByCoid(subscription.getCoid()).contains(subscription))
             return false;
-        }
+        subscriptionsRepository.save(subscription);
+        return true;
     }
 
     public boolean unsubscribe(Subscription subscription) {
@@ -41,13 +35,8 @@ public class SubscriberService {
         } else return false;
     }
 
-    public String getPlayerSubscriptions(Long pid) {
-        return new Gson().toJson(subscriptionsRepository.findByPid(pid));
-    }
-
-    public List<Subscription> getSubscriptionAnnouncements(Long pid) {
+    public List<Subscription> getPlayerSubscriptions(Long pid) {
         return subscriptionsRepository.findByPid(pid);
-
     }
 
     public Integer getSubscribersCount(Long coid) {
