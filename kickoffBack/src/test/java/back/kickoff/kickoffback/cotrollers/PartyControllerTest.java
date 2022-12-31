3b90d9d6c2e2f3b6d3698448216f5c0ce147e806
Omar khairat <omarkhairat133@gmail.com>
2022-12-31 06:23:22 +0200

    party controller tests added

--- kickoffBack/src/test/java/back/kickoff/kickoffback/cotrollers/PartyControllerTest.java
+++ kickoffBack/src/test/java/back/kickoff/kickoffback/cotrollers/PartyControllerTest.java
@@ -1,9 +1,18 @@
 package back.kickoff.kickoffback.cotrollers;
 
+import back.kickoff.kickoffback.Commands.CreateParty;
 import back.kickoff.kickoffback.services.PartyServices;
+import com.google.gson.Gson;
+import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,22 +23,79 @@ class PartyControllerTest {
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
         partyController = new PartyController(partyServices);
     }
 
     @Test
-    void createParty() {
+    void createParty() throws JSONException {
+        CreateParty createParty = new CreateParty(66L, "20", "5");
+        Mockito.when(partyServices.CreateParty(createParty)).thenReturn(Boolean.TRUE);
+        ResponseEntity<Boolean> response = partyController.CreateParty(createParty);
+        assertEquals(new ResponseEntity<>(Boolean.TRUE, HttpStatus.CREATED), response);
     }
 
     @Test
-    void joinParty() {
+    void joinParty() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.joinParty(information)).thenReturn(Boolean.FALSE);
+        ResponseEntity<Boolean> response = partyController.JoinParty(information);
+        assertEquals(new ResponseEntity<>(Boolean.FALSE, HttpStatus.BAD_REQUEST), response);
     }
 
     @Test
-    void leaveParty() {
+    void leaveParty() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.leaveParty(information)).thenReturn(Boolean.FALSE);
+        ResponseEntity<Boolean> response = partyController.LeaveParty(information);
+        assertEquals(new ResponseEntity<>(Boolean.FALSE, HttpStatus.BAD_REQUEST), response);
     }
 
     @Test
-    void deleteParty() {
+    void deleteParty() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.deleteParty(information)).thenReturn(Boolean.TRUE);
+        ResponseEntity<Boolean> response = partyController.DeleteParty(information);
+        assertEquals(new ResponseEntity<>(Boolean.TRUE, HttpStatus.CREATED), response);
+    }
+
+    @Test
+    void getPartiesCourtowner() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.getCourtOwnerParties(information)).thenReturn(new ArrayList<>());
+        ResponseEntity<String> response = partyController.GetPartiesCourtowner(information);
+        assertEquals(new ResponseEntity<>(new Gson().toJson(new ArrayList<>()), HttpStatus.OK), response);
+    }
+
+    @Test
+    void partiesCreatedbyPlayer() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.getPlayerCreatedParties(information)).thenReturn(new ArrayList<>());
+        ResponseEntity<String> response = partyController.PartiesCreatedbyPlayer(information);
+        assertEquals(new ResponseEntity<>(new Gson().toJson(new ArrayList<>()), HttpStatus.OK), response);
+    }
+
+    @Test
+    void partiesJoinedbyPlayer() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.getPartiesofplayerJoined(information)).thenReturn(new ArrayList<>());
+        ResponseEntity<String> response = partyController.PartiesJoinedbyPlayer(information);
+        assertEquals(new ResponseEntity<>(new Gson().toJson(new ArrayList<>()), HttpStatus.OK), response);
+    }
+
+    @Test
+    void playersJoinedParties() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.getplayersofParties(information)).thenReturn(new ArrayList<>());
+        ResponseEntity<String> response = partyController.PlayersJoinedParties(information);
+        assertEquals(new ResponseEntity<>(new Gson().toJson(new ArrayList<>()), HttpStatus.OK), response);
+    }
+
+    @Test
+    void partiesNotJoinedbySubscribers() throws JSONException {
+        String information = "";
+        Mockito.when(partyServices.getPartiesSubscribed(information)).thenReturn(new ArrayList<>());
+        ResponseEntity<String> response = partyController.PartiesNotJoinedbySubscribers(information);
+        assertEquals(new ResponseEntity<>(new Gson().toJson(new ArrayList<>()), HttpStatus.OK), response);
     }
 }
