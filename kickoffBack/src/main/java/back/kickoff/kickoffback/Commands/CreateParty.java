81050d5ae9a282846c01291c6509259303811623
Omar khairat <omarkhairat133@gmail.com>
2022-12-31 05:58:17 +0200

    party model test added

--- kickoffBack/src/main/java/back/kickoff/kickoffback/Commands/CreateParty.java
+++ kickoffBack/src/main/java/back/kickoff/kickoffback/Commands/CreateParty.java
@@ -1,12 +1,21 @@
 package back.kickoff.kickoffback.Commands;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.validation.constraints.NotNull;
+
+@EqualsAndHashCode
+@AllArgsConstructor
 public class CreateParty {
 
+    @NotNull
     public Long reservationId;
+    @NotNull
     public String neededNumbers;
+    @NotNull
     public String availableNumbers;
     public CreateParty(String jsonParty) throws JSONException {
         JSONObject jsonObject = new JSONObject(jsonParty);
